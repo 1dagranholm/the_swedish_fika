3473316992c63c71bdf594eb688982c63bf7b935
Per Egertun <per.egertun@chasacademy.se>
2020-01-30 14:51:04 +0100

    add merged files

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Order;
 use DB;
 
 class OrderController extends Controller {
@@ -13,5 +14,30 @@ class OrderController extends Controller {
         return view('orders', [
             'order' => $order
         ]);
+    }
+
+    public function cart(Request $request) {
+
+        $product = $request;
+        $total = 0;
+        foreach ($product['product'] as $products) {
+
+            if($products['count'] > 0) {
+                $total = ($total + ($products['price'] * $products['count']));
+            }
+        }
+
+        return view('cart', [
+            'product' => $request->all(),
+            'total' => $total
+        ]);
+    }
+
+    public function confirm(Request $request) {
+
+        $products = $request->all();
+        Order::confirm($products);
+
+        return view('confirm');
     }
 }
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -4,25 +4,45 @@
 
 <h1 class="page-header">Kundkorg</h1>
 <table class="table table-striped table-hover table-responsive-sm">
+<form action="/confirm" id="cart_form" method="POST">
+        @csrf
         <thead>
             <tr>
             <th scope="col">ID</th>
             <th scope="col">Namn</th>
-            <th scope="col">Smaker</th>
+            <th scope="col">Smak</th>
             <th scope="col">Produkttyp</th>
-            <th scope="col">Pris</th>
+            <th scope="col">Antal</th>
+            <th scope="col">Á Pris</th>
+            <th scope="col">Total</th>
             </tr>
         </thead>
-
         <tbody>
+
+            @foreach ($product['product'] as $products)
+            @if($products['count'] > 0)
             <tr>
-            <th scope="row">-</th>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>- SEK</td>
+                <th scope="row"> {{ $products['id'] }} </th>
+                <td> {{ $products['name'] }} </td>
+                <td> {{ $products['flavor'] }} </td>
+                <td><{{ $products['type'] }} </td>
+                <td>{{ $products['count'] }}</td>
+                <td>{{ $products['price'] }}</td>
+                <td>{{ $products['price'] * $products['count'] }}</td>
+            </tr>
+            @endif
+            @endforeach
+
+            <tr>
+                <td>Totalt för order: </td>
+                <td> {{ $total }} </td>
+            </tr>
+
+            <tr>
+                <td><input type="submit" value="Bekräfta beställningen"></td>
             </tr>
         </tbody>
+    </form>
 </table>
 
 <a href="#" class="btn darker-pink-bgr">Köp</a>
