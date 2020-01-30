dacd02b64253b39ae8210a7a248dd4cb10276f09
Johan Abde <johanabde@gmail.com>
2020-01-30 11:10:30 +0100

    Add 'required' to searchfield in layout.blade

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -41,7 +41,7 @@
           </ul>
           <form class="form-inline my-2 my-lg-0" action="{{url('/search')}}" method="post">
           {{ csrf_field() }}
-              <input class="form-control mr-sm-2" type="search" placeholder="Sök" name="searchInput">
+              <input class="form-control mr-sm-2" type="search" placeholder="Sök" name="searchInput" required>
               <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sök</button>
           </form>
         </div>
