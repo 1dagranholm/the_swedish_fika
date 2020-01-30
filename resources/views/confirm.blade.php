6bd888036fc937e3706c8f9c97db2e84178db830
Ida Granholm <granholm.ida@gmail.com>
2020-01-30 18:37:22 +0100

    Fix confirm page

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -1,7 +1,15 @@
 @extends ('layout')
 
 @section ('content')
-
-    <h1 class="title">Order bekräftad!</h1>
+<div class="jumbotron jumbotron-fluid text-dark pt-0 donuts-img" id="header-image">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 text-center bg-dark">
+                    <h1 class="page-header text-white mt-3">Tack för din order!</h1>
+                    <p class="text-center text-white">Vi bekräfter härmed din order. Den kommer att färdigställas och skickas till dig inom kort. Hoppas att det smakar!</p>
+            </div>  
+        </div>
+    </div>
+</div>
 
 @endsection
