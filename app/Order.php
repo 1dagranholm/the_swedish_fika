d7546edd68371c7608578be9dfeeb5b7017098bf
Jonna <jonnaliesel@gmail.com>
2020-01-30 17:25:33 +0100

    Forst attempt to insert to orders table

--- app/Order.php
+++ app/Order.php
@@ -1,12 +1,24 @@
 <?php
 
 namespace App;
+use DB;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
     public static function confirm($products) {
-        //insert $products to DB
+
+        foreach($products as $product) {
+            $id = $product['id'];
+            $count = $product['count'];
+
+            $order = [
+                'product_id' => $id,
+                'count' => $count
+            ];
+        }
+
+        DB::table('orders')->insert($order);
     }
 }
