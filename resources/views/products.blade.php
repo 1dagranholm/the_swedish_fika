0f89dcd4f62bba5b64bbbc1168029c6a1e6b1739
Ida Granholm <granholm.ida@gmail.com>
2020-01-30 18:13:23 +0100

    Fix removal of visible input fields on product page

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -19,16 +19,16 @@
 
             @foreach ($product as $products)
                 <tr>
-                <th scope="row"><input name="product[{{ $products->id }}][id]" value="{{ $products->id }}" /></th>
-                <td><input readonly name="product[{{ $products->id }}][name]" value="{{ $products->name }}" /></td>
-                <td><input readonly name="product[{{ $products->id }}][flavor]" value="{{ $products->flavor }}" /></td>
-                <td><input readonly name="product[{{ $products->id }}][type]" value="{{ $products->type }}" /></td>
-                <td><input readonly name="product[{{ $products->id }}][price]" value="{{ $products->price }}" /></td>
+                <th scope="row">{{ $products->id }}<input hidden readonly name="product[{{ $products->id }}][id]" value="{{ $products->id }}" /></th>
+                <td>{{ $products->name }}<input hidden readonly name="product[{{ $products->id }}][name]" value="{{ $products->name }}" /></td>
+                <td>{{ $products->flavor }}<input hidden readonly name="product[{{ $products->id }}][flavor]" value="{{ $products->flavor }}" /></td>
+                <td>{{ $products->type }}<input hidden readonly name="product[{{ $products->id }}][type]" value="{{ $products->type }}" /></td>
+                <td>{{ $products->price }}<input hidden readonly name="product[{{ $products->id }}][price]" value="{{ $products->price }}" /></td>
                 <td><input name="product[{{ $products->id }}][count]" max="10" min="0" type="number" value="0"></td>
                 </tr>
             @endforeach
                 <tr>
-                    <td><input type="submit" value="Lätt till i kundkorgen"></td>
+                    <td colspan="6" class="text-right bg-white"><input class="btn darker-pink-bgr" type="submit" value="Lätt till i kundkorgen"></td>
                 </tr>
             </tbody>
     </form>
