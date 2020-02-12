2ec6db2c0f63d98013c292a7e5f5acbc879a20ff
Per Egertun <per.egertun@chasacademy.se>
2020-02-12 19:31:15 +0100

    add testing api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,10 +8,11 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller {
 
-    public function show() {
+    public function show(Product $products) {
         $products = Product::show();
-        return view('products', [
-            'product' => $products
-        ]);
+        // return view('products', [
+        //     'product' => $products]);
+        return $products;
+        
     }
 }
